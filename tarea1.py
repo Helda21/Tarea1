c41b6645f326b4bdd77ac26ebe4cfca64542d84e
Helda21 <92833018+Helda21@users.noreply.github.com>
2022-07-25 13:46:49 -0500

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,8 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hola, soy Helio")
+Hola, soy Helio
+print("Estoy empezando el curso de python")
+Estoy empezando el curso de python
+print("Espero aprender mucho")
+Espero aprender mucho
